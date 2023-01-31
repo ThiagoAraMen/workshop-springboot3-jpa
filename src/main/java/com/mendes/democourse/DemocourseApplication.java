f6d25bb3151cec5b98e2e8b73d34031da174e602
Thiago Mendes <thiago.aramen@gmail.com>
2023-01-31 09:37:43 -0300

    Project created

---
+++ src/main/java/com/mendes/democourse/DemocourseApplication.java
@@ -0,0 +1,13 @@
+package com.mendes.democourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemocourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mendes/democourse/DemocourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mendes.democourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
